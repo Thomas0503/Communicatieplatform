7e720eb847752d8a994dbf206a275d7c2baf133c
RubenMinJou <ruben.minjou@student.kuleuven.be>
2021-10-30 16:50:25 +0200

    Auto stash before rebase of "origin/App"

--- app_communicatieplatfor/app/src/main/java/com/communicatieplatform/Activiteit.java
+++ app_communicatieplatfor/app/src/main/java/com/communicatieplatform/Activiteit.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
 public class Activiteit extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activiteit);
-
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
     }
 }
--- app_communicatieplatfor/app/src/main/java/com/communicatieplatform/Kalender.java
+++ app_communicatieplatfor/app/src/main/java/com/communicatieplatform/Kalender.java
@@ -3,12 +3,28 @@ package com.communicatieplatform;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Kalender extends AppCompatActivity {
     private Button button;
 
+    private FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +40,25 @@ public class Kalender extends AppCompatActivity {
     }
 
     public void openAfspraak() {
-        Intent intent = new Intent(this, Afspraak.class);
-        startActivity(intent);
+        //Intent intent = new Intent(this, Afspraak.class);
+        //startActivity(intent);
+        db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("cities").document("LA");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d("TAG", "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d("TAG", "No such document");
+                    }
+                } else {
+                    Log.d("TAG", "get failed with ", task.getException());
+                }
+            }
+        });
+
     }
 }
