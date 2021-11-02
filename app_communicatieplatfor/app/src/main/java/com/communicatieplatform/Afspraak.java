069fa6a25aa10240dbb940a31897f769dde7e9cc
RubenMinJou <ruben.minjou@student.kuleuven.be>
2021-11-02 13:03:54 +0100

    recyclerview tegevoegd

--- app_communicatieplatfor/app/src/main/java/com/communicatieplatform/Afspraak.java
+++ app_communicatieplatfor/app/src/main/java/com/communicatieplatform/Afspraak.java
@@ -1,15 +1,2 @@
-package com.communicatieplatform;
-
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-public class Afspraak extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.afspraak);
-    }
+package com.communicatieplatform;public class Afspraak {
 }
---
+++ app_communicatieplatfor/app/src/main/java/com/communicatieplatform/AfspraakMaken.java
@@ -0,0 +1,15 @@
+package com.communicatieplatform;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Afspraak extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.afspraak);
+    }
+}
